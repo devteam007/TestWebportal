5a6a65deea0bb83ac5d02d07d577395a57e91ba7
Aditya <150101480@svuca.edu>
2017-07-12 09:09:12 -0400

    MasterBranchTestWebportal

---
+++ Documents/Visual Studio 2015/Projects/WebPortal/WebPortal/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebPortal.Models;
+
+namespace WebPortal.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult SignUp()
+        {
+           
+            return View("SignUp");
+        }
+
+        public ActionResult LogIn()
+        {
+
+            return View();
+        }
+
+        #region Login Page Submit
+        public ActionResult Submit()
+        {
+
+            return View("LogIn");
+        }
+
+
+        #endregion
+
+
+
+
+    }
+}
---
+++ Documents/Visual Studio 2015/Projects/WebPortal/WebPortal/Models/LogIn.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebPortal.Models
+{
+    public class LogIn
+    {
+        [Display(Name ="UserName")]
+        [Required(ErrorMessage ="Please enter User Name")]
+        public string UserName { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "Please enter Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "FirstName")]
+        [Required(ErrorMessage = "Please enter First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "LastName")]
+        [Required(ErrorMessage = "Please enter Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "EmailId")]
+        [Required(ErrorMessage = "Please enter Email Id")]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Invalid Email")]
+        public string EmailId { get; set; }
+
+        [Display(Name = "ContactNumber")]
+        [Required(ErrorMessage = "Please enter Contact Number")]
+        public int ContactNumber { get; set; }
+        public string Address { get; set; }
+    }
+}
